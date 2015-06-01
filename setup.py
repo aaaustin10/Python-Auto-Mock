cc202836cd762c49cf6971c6ff01061e693c5864
Austin Stewart <aaaustin10@gmail.com>
2015-06-01 11:50:45 -0500

    Update README.rst, add license, and fix MANIFEST.in

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ def read(*paths):
         return f.read()
 
 setup(
-    name='pythonAutoMock',
+    name='python-auto-mock',
     version='0.1.0',
-    description='A module to easily mock code.',
-    long_description=(read('README.rst') + '\n\n' +
-                      read('HISTORY.rst')),
-    url='https://github.com/aaaustin10/Python-Easy-Mock',
+    description='A module to easily mock code while testing.',
+    long_description=(read('README.rst')),
+    url='https://github.com/aaaustin10/Python-Auto-Mock',
     license='MIT',
     author='Austin Stewart',
+    author_email='austinfstewart@gmail.com',
     py_modules=['auto_mock'],
     include_package_data=True,
     classifiers=[
@@ -25,7 +25,11 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
